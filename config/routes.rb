3743579bde65baafa58893154b0716af053d18b7
David Deegan <daviddeegan@gmail.com>
2025-06-09 18:13:31 +0700

    rails generate authentication --skip-test-framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "pages/home"
   get "pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
