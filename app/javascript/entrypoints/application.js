692f5ef1ff406b49389acdcee58f0c5380fb2007
David Deegan <daviddeegan@gmail.com>
2025-06-12 20:22:17 +0700

    Increase height of empty chart container in reports view for better visibility

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -187,7 +187,7 @@ function showEmptyChart(chartId, message = 'No data available') {
   const container = document.getElementById(chartId);
   if (container) {
     container.innerHTML = `
-      <div class="flex items-center justify-center h-64">
+      <div class="flex items-center justify-center h-80">
         <div class="text-center">
           <svg class="w-12 h-12 text-gray-300 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path>
